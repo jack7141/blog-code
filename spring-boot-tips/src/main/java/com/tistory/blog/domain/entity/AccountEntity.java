49a10dd1bb6b8410ba5afa0debdda124629835d6
황광회 <jack7141@nate.com>
2024-11-04 18:33:22 +0900

    Builder 패턴을 활용하여 객채 생성 패턴

--- spring-boot-tips/src/main/java/com/tistory/blog/domain/entity/AccountEntity.java
+++ spring-boot-tips/src/main/java/com/tistory/blog/domain/entity/AccountEntity.java
@@ -7,13 +7,13 @@ import lombok.*;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 //@AllArgsConstructor
-@Getter
 //@Builder
 //@Component
 @Entity
 @Table(name = "account")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class AccountEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -41,14 +41,36 @@ public class AccountEntity {
 //    }
 
     // 안전한 객채 생성 패턴
-    @Builder
+//    @Builder
+//    public AccountEntity(String bankName, String accountNumber, String accountHolder) {
+//        Assert.hasText(bankName, "bankName 값이 누락되었습니다.");
+//        Assert.hasText(accountNumber, "accountNumber 값이 누락되었습니다.");
+//        Assert.hasText(accountHolder, "accountHolder 값이 누락되었습니다.");
+//
+//        this.bankName = bankName;
+//        this.accountNumber = accountNumber;
+//        this.accountHolder = accountHolder;
+//    }
+
+    // 안전한 객채 생성 이름 부여
+    @Builder(builderClassName = "CreditAccountBuilder", builderMethodName = "creditAccountBuilder")
     public AccountEntity(String bankName, String accountNumber, String accountHolder) {
-        Assert.hasText(bankName, "bankName must not be empty");
-        Assert.hasText(accountNumber, "accountNumber must not be empty");
-        Assert.hasText(accountHolder, "accountHolder must not be empty");
+        Assert.hasText(bankName, "bankName 값이 누락되었습니다.");
+        Assert.hasText(accountNumber, "accountNumber 값이 누락되었습니다.");
+        Assert.hasText(accountHolder, "accountHolder 값이 누락되었습니다.");
 
         this.bankName = bankName;
         this.accountNumber = accountNumber;
         this.accountHolder = accountHolder;
+    }
+
+    @Override
+    public String toString() {
+        return "AccountEntity{" +
+                "id=" + id +
+                ", bankName='" + bankName + '\'' +
+                ", accountNumber='" + accountNumber + '\'' +
+                ", accountHolder='" + accountHolder + '\'' +
+                '}';
     }
 }
--- spring-boot-tips/src/test/java/com/tistory/blog/domain/entity/AccountEntityTest.java
+++ spring-boot-tips/src/test/java/com/tistory/blog/domain/entity/AccountEntityTest.java
@@ -3,13 +3,51 @@ package com.tistory.blog.domain.entity;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AccountEntityTest {
+
+    String BANKNAME = "우리은행";
+    String ACCOUNTNUMBER = "123-123-123-123";
+    String ACCOUNTHOLDER = "ME";
+
     @Test
-    @DisplayName(value = "안전한 객채 생성 패턴")
-    void createAccountEntity() {
-        AccountEntity accountEntity = AccountEntity.builder().build();
+    @DisplayName(value = "불안전한 객채 생성 패턴")
+    void createUnstableAccountEntity() {
+        AccountEntity accountEntity = AccountEntity.creditAccountBuilder()
+                .accountHolder("")
+                .accountNumber(ACCOUNTNUMBER)
+                .bankName(BANKNAME)
+                .build();
+        assertThat(accountEntity.getBankName()).isEqualTo(BANKNAME);
+        assertThat(accountEntity.getAccountNumber()).isEqualTo(ACCOUNTNUMBER);
+        System.out.println("AccountEntity 객체 : " + accountEntity.toString());
     }
 
+    @Test
+    @DisplayName(value = "안정적인 객채 생성 패턴")
+    void createstableAccountEntity() {
+        AccountEntity accountEntity = AccountEntity.creditAccountBuilder()
+                .accountHolder(BANKNAME)
+                .accountNumber(ACCOUNTNUMBER)
+                .bankName(BANKNAME)
+                .build();
+        assertThat(accountEntity.getBankName()).isEqualTo(BANKNAME);
+        assertThat(accountEntity.getAccountNumber()).isEqualTo(ACCOUNTNUMBER);
+        System.out.println("AccountEntity 객체 : " + accountEntity.toString());
+    }
+
+    @Test
+    @DisplayName(value = "안정적인 객채 생성 빌더 이름 명명")
+    void createAccountEntityBuilderName() {
+        AccountEntity accountEntity = AccountEntity.creditAccountBuilder()
+                .accountHolder("")
+                .accountNumber(ACCOUNTNUMBER)
+                .bankName(BANKNAME)
+                .build();
+        assertThat(accountEntity.getBankName()).isEqualTo(BANKNAME);
+        assertThat(accountEntity.getAccountNumber()).isEqualTo(ACCOUNTNUMBER);
+        System.out.println("AccountEntity 객체 : " + accountEntity.toString());
+    }
 }
