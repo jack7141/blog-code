5c5f71998953fa3f533c72a440d48deae0b10a04
황광회 <ghl92479@gmail.com>
2025-04-07 14:31:29 +0900

    ADD: Test Code

--- class-101/users/tests.py
+++ class-101/users/tests.py
@@ -11,7 +11,20 @@ class UserModelTest(TestCase):
             'user_id': 'test123',
         }
         self.user = User.objects.create(**self.user_data)
+"""
+기본적으로 TestCase는 테스트마다 트랜잭션을 감싸고 테스트 후 롤백하기 때문에 실제 DB에는 영구적으로 insert되지 않는다. 
+그래서 테스트 중 생성된 유저는 테스트가 끝나면 사라진다. -> 실제로 Insert까지 확인하려면 TransactionTestCase과 serialized_rollback를 활용
 
+python manage.py shell
+from users.models import User
+t = User.objects.create(username="test123", user_id="real1235")
+t.delete()
+
+* User = get_user_model()
+Django 공식 권장사항: Django 공식 문서에서는 get_user_model()을 통해 활성화된 User 모델을 가져오도록 권장하고 있습니다.
+커스텀 User 모델 지원: Django는 AUTH_USER_MODEL 설정을 통해 프로젝트에서 기본 User 모델을 다른 커스텀 모델로 대체할 수 있게 해줍니다. 
+get_user_model()은 항상 현재 활성화된 User 모델을 반환하므로, 기본 모델이든 커스텀 모델이든 상관없이 올바른 User 클래스를 참조할 수 있습니다.
+"""
     def test_create_user(self):
         """사용자 생성 테스트"""
         # 사용자가 정상적으로 생성되었는지 확인
