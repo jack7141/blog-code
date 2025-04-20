77e1411b705f254e46cf2d5b3b432b650ae3fe2d
황광회 <jack7141@nate.com>
2025-04-20 23:05:59 +0900

    Create Var2.java

---
+++ java-basic/java-basic/src/variable/Var2.java
@@ -0,0 +1,15 @@
+package variable;
+
+public class Var2 {
+    public static void main(String [] args) {
+        int a;
+        double b;
+        boolean c;
+//        char은 작은 따옴표
+        char d = 'A';
+//        String은 큰 따옴표
+        String e = "Hello World";
+        System.out.println(e);
+        System.out.println(d);
+    }
+}
