89f5a16cf4f475b32f0cf156c3cbea7ad2155923
황광회 <jack7141@nate.com>
2025-04-20 22:59:13 +0900

    Java 기초 추가

---
+++ java-basic/java-basic/src/variable/Var1.java
@@ -0,0 +1,13 @@
+package variable;
+
+public class Var1 {
+
+    public static void main(String[] args) {
+        int a;
+        int b;
+        a = 10;
+        b = 10;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
